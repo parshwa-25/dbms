80b61d12ec856f573d7aac926a9db284d12140d0
parshwa-25 <kavy122007@gmail.com>
2026-02-10 23:45:43 +0530

    Created 1st Table

--- project.sql
+++ project.sql
@@ -1,7 +1,7 @@
 create table customer (
-    id number(5),
-    name varchar(20),
-    addr varchar(50)
+    id number(5) primary key,
+    name varchar(20) not null,
+    mob_no number(10) not null,
+    email varchar(30) not null,
+    city varchar(20) not null
 );
-
-https://youtu.be/N_bMCff8q6A
